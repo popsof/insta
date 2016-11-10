1a67a6bfe3da8cfd4bc1b55378386049976f8048
popsof <popsof@gmail.com>
2016-11-10 17:06:16 +0900

    Add app member, photo

--- insta/insta/settings.py
+++ insta/insta/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'member',
+    'photo',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -84,6 +86,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
+AUTH_USER_MODEL = "member.MyUser"
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
---
+++ insta/member/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, UserManager
+
+class MyUserManager(UserManager):
+    pass
+
+class MyUser(AbstractUser):
+    pass
+# Create your models here.
